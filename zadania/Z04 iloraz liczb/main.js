e15b8fbd5eca67315fd0efa2d5e23108d2fbb3ee
JakubPien <jakub.pieniezny@onet.pl>
2024-09-23 15:08:33 +0200

    Zadanie Z05 - zakonczone

--- zadania/Z04 iloraz liczb/main.js
+++ zadania/Z04 iloraz liczb/main.js
@@ -1,12 +1,12 @@
 const liczba_a = document.querySelector(`#a`)
 const liczba_b = document.querySelector(`#b`)
-const wynik = document.querySelector(`#wynik`)
+const wynik = document.querySelector(`.wynik`)
 
 const btn = document.querySelector(`button`)
 
 btn.addEventListener(`click`, function (){
-    let a = parseFloat(liczba_a)
-    let b = parseFloat(liczba_b)
+    let a = parseFloat(liczba_a.value)
+    let b = parseFloat(liczba_b.value)
 
     let iloraz = a / b
     wynik.innerHTML =
