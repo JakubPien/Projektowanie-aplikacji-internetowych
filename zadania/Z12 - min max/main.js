cd8afef1f05dac511387cb6f6284dfe32571f8a7
JakubPien <jakub.pieniezny@onet.pl>
2024-10-07 15:52:00 +0200

    Z14 - skończone

---
+++ zadania/Z12 - min max/main.js
@@ -0,0 +1,36 @@
+const mi = document.querySelector("#min")
+const ma = document.querySelector("#max")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", function() {
+    let min = parseFloat(mi.value)
+    let max = parseFloat(ma.value)
+
+    let a = Math.floor( Math.random() * (max-min) + min)
+    let b = Math.floor( Math.random() * (max-min) + min)
+    let c = Math.floor( Math.random() * (max-min) + min)
+    let d = Math.floor( Math.random() * (max-min) + min)
+    let e = Math.floor( Math.random() * (max-min) + min)
+
+    let suma = a + b + c + d + e
+    let iloczyn = a * b * c * d * e
+    let srednia = (a + b + c + d + e) / 5
+
+    wynik.innerHTML =
+        `
+        Wylosowane liczby:
+        <ul>
+        <li>${a}</li>
+        <li>${b}</li>
+        <li>${c}</li>
+        <li>${d}</li>
+        <li>${e}</li>
+        </ul>
+        
+        Suma liczb: ${a} + ${b} + ${c} + ${d} + ${e} = ${suma}
+        Iloczyn liczb: ${a} * ${b} * ${c} * ${d} * ${e} = ${iloczyn}
+        Średnia liczb = ${srednia}
+        `
+
+
+})
