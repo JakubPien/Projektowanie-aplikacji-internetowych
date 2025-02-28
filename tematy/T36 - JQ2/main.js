7500c11bf7a62a30049c6eb1c8bec5b6ae280aff
JakubPien <jakub.pieniezny@onet.pl>
2025-02-28 16:48:15 +0100

    Zadania 35, 36 i 37 skończone

--- tematy/T36 - JQ2/main.js
+++ tematy/T36 - JQ2/main.js
@@ -1,26 +1,42 @@
 $(function() {
+        $('#tekst1').hide("slow")
+        $('#tekst2').hide("slow");
+        $('#tekst3').hide("slow");
+        $('#tekst4').hide("slow");
+});
+
+$(function() {
         $('#tekst_1').click(function() {
                 $('#tekst1').toggle("slow");
                 $('#tekst2').hide("slow");
                 $('#tekst3').hide("slow");
                 $('#tekst4').hide("slow");
         });
+});
+
+$(function() {
         $('#tekst_2').click(function() {
-                $('#tekst1').hidePopover("slow");
+                $('#tekst1').hide("slow");
                 $('#tekst2').toggle("slow");
-                $('#tekst3').hidePopover("slow");
-                $('#tekst4').hidePopover("slow");
+                $('#tekst3').hide("slow");
+                $('#tekst4').hide("slow");
         });
+});
+
+$(function() {
         $('#tekst_3').click(function() {
-                $('#tekst1').hidePopover("slow");
-                $('#tekst2').hidePopover("slow");
+                $('#tekst1').hide("slow");
+                $('#tekst2').hide("slow");
                 $('#tekst3').toggle("slow");
-                $('#tekst4').hidePopover("slow");
+                $('#tekst4').hide("slow");
         });
+});
+
+$(function() {
         $('#tekst_4').click(function() {
-                $('#tekst1').hidePopover("slow");
-                $('#tekst2').hidePopover("slow");
-                $('#tekst3').hidePopover("slow");
+                $('#tekst1').hide("slow");
+                $('#tekst2').hide("slow");
+                $('#tekst3').hide("slow");
                 $('#tekst4').toggle("slow");
         });
 });
